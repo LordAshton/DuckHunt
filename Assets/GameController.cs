cbc4945d320e56eea218d117c3f3c6ef08ea6c3a
Ashton Moyer <lordashtonthecool@hotmail.com>
2014-12-02 15:25:31 -0700

    Made the Duck Hunt
    Uploading the Duck Hunt game.

---
+++ Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public Camera cam;
+
+	private float maxWidth;
+
+	void Start () {
+		if (cam == null) {
+			cam = Camera.main;
+		}
+
+
+
+ //public float flightspeed
+
+	//public virtual void OnMouseEnter ()
+	//{
+	//StaticVars.gameScore++;
+	//Object.Destroy (this.gameObject);
+	//}
+
+	//public static int gameScore = 0
+
+	//public GameObject
+	}
+}
---
+++ Assets/crosshaircontroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crosshaircontroller : MonoBehaviour {
+	
+	public Camera cam;
+	
+	void Start () {
+		if (cam == null) {
+			cam = Camera.main;
+		}
+	}
+	
+	void FixedUpdate () {
+		Vector3 rawPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+		rigidbody2D.MovePosition (rawPosition);
+
+		
+	}
+}
